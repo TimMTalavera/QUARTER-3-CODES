d808c3c033d654b2a0487f6675601030d049af2e
TimMTalavera <timzyker.talavera@smc.pshs.edu.ph>
2026-01-26 09:44:30 +0800

    Create Talavera_Q3_FA06.py

---
+++ Talavera_Q3_FA06.py
@@ -0,0 +1,18 @@
+# Create an empty dictionary
+student = {}
+
+# Collect student information
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+subject = input("Enter your favorite subject: ")
+
+student["name"] = name
+student["age"] = age
+student["subject"] = subject
+
+
+# Display the student record
+print("\nStudent Record:")
+print("Name:", name)
+print("Age:", age)
+print("Favorite Subject:", subject)
